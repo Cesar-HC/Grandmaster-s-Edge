d418cd38f89d722fec48bf328fb0de9dcd3b329a
Cesar <u20231e202@upc.edu.pe>
2026-07-25 06:28:10 -0500

    Corrección de WebSocketConfig.java

--- src/main/java/com/upc/ajedrezbackend/config/WebSocketConfig.java
+++ src/main/java/com/upc/ajedrezbackend/config/WebSocketConfig.java
@@ -24,20 +24,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws-chess")
                 .setAllowedOriginPatterns("*");
     }
-
-    @Override
-    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
-        registration.setMessageSizeLimit(50 * 1024 * 1024);
-        registration.setSendBufferSizeLimit(50 * 1024 * 1024);
-        registration.setSendTimeLimit(20000);
-    }
-
-    @Bean
-    public ServletServerContainerFactoryBean createWebSocketContainer() {
-        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
-        container.setMaxTextMessageBufferSize(50 * 1024 * 1024);
-        container.setMaxBinaryMessageBufferSize(50 * 1024 * 1024);
-        container.setMaxSessionIdleTimeout(300000L);
-        return container;
-    }
 }
